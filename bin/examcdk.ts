e04a1eefabf859fc069ede37a2ea8299695101b7
EC2 Default User <ec2-user@ip-172-31-2-94.us-east-2.compute.internal>
2023-09-28 02:13:46 +0000

    first message

--- bin/examcdk.ts
+++ bin/examcdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ExamcdkStack } from '../lib/examcdk-stack';
+import { MyCdkStack } from '../lib/examcdk-stack';
 
 const app = new cdk.App();
-new ExamcdkStack(app, 'ExamcdkStack', {
+new MyCdkStack(app, 'MyCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/examcdk-stack.ts
+++ lib/examcdk-stack.ts
@@ -1,16 +1,62 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class ExamcdkStack extends cdk.Stack {
+export class MyCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC
+    const vpc = new ec2.Vpc(this, 'MyVPC', {
+      cidr: '10.30.0.0/16',
+      maxAzs: 3, // Change this as needed
+      natGateways: 1,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExamcdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a Security Group for the EC2 instance
+    const securityGroup = new ec2.SecurityGroup(this, 'MyEC2SecurityGroup', {
+      vpc,
+    });
+
+    // Create an EC2 instance in the public subnet
+    const ec2Instance = new ec2.Instance(this, 'MyEC2Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      securityGroup,
+      machineImage: ec2.MachineImage.latestAmazonLinux({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+    });
+
+    // Create an SQS Queue
+    const queue = new sqs.Queue(this, 'MyQueue');
+
+    // Create an SNS Topic
+    const topic = new sns.Topic(this, 'MyTopic');
+
+    // Create AWS Secrets Manager Secret
+    const secret = new secretsmanager.Secret(this, 'MySecret', {
+      secretName: 'metrodb-secrets',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'my-username' }),
+        generateStringKey: 'password',
+        passwordLength: 12,
+      },
+    });
+
+    // Grant necessary permissions to resources
+    ec2Instance.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['secretsmanager:GetSecretValue'],
+        resources: [secret.secretArn],
+      })
+    );
+
+    // queue.grantSendMessages(topic);
   }
 }
+
+// const app = new cdk.App();
+// new MyCdkStack(app, 'MyCdkStack');
